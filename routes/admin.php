7496d365349356fb337d1a62971d0a972b03f14c
DilawarSafar <dilawar.king0@gmail.com>
2022-10-07 14:47:15 +0500

    route update

---
+++ routes/admin.php
@@ -0,0 +1,19 @@
+<?php
+
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\DueController;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\VisitorController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('admin')->middleware('auth')->group(function(){
+    Route::get('/dashboard', [UserController::class,'index'])->name('dashboard');
+    Route::resource('user', UserController::class);
+    Route::resource('visitor', VisitorController::class);
+    Route::resource('student',StudentController::class);
+    Route::resource('dues',DueController::class);
+
+});
+
