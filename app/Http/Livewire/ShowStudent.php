899b78b633a2fc36e3ef6fe9bb98b3ce1815e522
Shahzad Farooq <crindalwalt@protonmail.com>
2022-10-10 15:27:43 +0500

    visitor searchbar converted as livewire component (SF-12)

---
+++ app/Http/Livewire/ShowStudent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ShowStudent extends Component
+{
+    public function render()
+    {
+        return view('livewire.show-student');
+    }
+}
